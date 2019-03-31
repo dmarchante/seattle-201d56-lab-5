13c38ec370f7dee21d505d8db35149c704305594
David Marchante <cdmarchante@gmail.com>
2019-03-31 09:30:00 -0700

    fix problem4; add solution to problem-5

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -127,11 +127,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let output = [];
+  let result = 0;
+  for(let i = 0; i < multArr.length; i++) {
+    result *= multArr[i];
+  }
 
+  const message = `${multArr[0]},${multArr[1]},${multArr[2]} was passed in as an array of numbers, and 9 is their sum.`;
+
+  output.push(result, message);
+
+  return output;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray); //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop
 
